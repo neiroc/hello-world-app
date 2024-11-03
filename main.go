94f3490b713a46a013d50207adca3daea9c374a7
neiroc <emarchel@proton.me>
2024-11-03 17:09:00 +0100

    fix: deployment

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, World! This is a test. Number 2")
+    fmt.Fprintf(w, "Hello, World! This is a test. Number 3")
 }
 
 func main() {
